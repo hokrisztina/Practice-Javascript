44dc1e60a7f233aa937d9d3a46a873587982c52e
hokrisztina <72090744+hokrisztina@users.noreply.github.com>
2020-10-02 16:28:16 +0200

    my practice so far

---
+++ scripts.js
@@ -0,0 +1,32 @@
+let age = 27;
+let ww2 = 1945-1939;
+let tesla = 76*370.30;
+let secondsOfDay = 60*60*24;
+let hungarians = 10000000 / 7800000000000 *100;
+let bigger = 9597000000 / 78865;
+let participants = 654%7;
+console.log(age)
+console.log(ww2)
+console.log(tesla)
+console.log(secondsOfDay)
+console.log(hungarians)
+console.log(bigger)
+console.log(participants)
+
+let name = 'Mark';
+let greeting = `Hello ${name}!`;
+console.log(greeting)
+let greetworld = 'Hello World';
+console.log(greetworld.length)
+let names = ['Julia', 'Peter', 'Chandler', 'Monica', 'Rachel', 'Joey']
+trueOrFalse = names[4].length > 5;
+console.log(trueOrFalse)
+names.push = 'Ross';
+console.log(names)
+names[0] ='Krisztina';
+console.log(names)
+trueOrFalse = names.length > 4;
+console.log(trueOrFalse)
+let post = {
+    color: 
+}
